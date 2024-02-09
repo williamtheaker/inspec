a65a3642643bbd472cbb24f48340bde4e03c2894
Sonu Saha <sonu.saha@progress.com>
2024-02-09 16:03:05 +0530

    test sbom generation
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- omnibus/omnibus-test.sh
+++ omnibus/omnibus-test.sh
@@ -11,3 +11,23 @@ PATH=/opt/inspec/bin:/opt/inspec/embedded/bin:$PATH
 export PATH
 
 rake
+
+echo "Generating SBOM"
+install_dir="/opt/inspec"
+
+# Install syft
+echo "Installing syft"
+curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
+
+# Check syft version
+echo "Checking syft version"
+/usr/local/bin/syft version
+
+# Generate SBOM
+echo "Generating SBOM"
+/usr/local/bin/syft "$install_dir" --output spdx-json > "$install_dir"/embedded/inspec-sbom.json
+
+# Display SBOM
+echo "Displaying SBOM"
+cat "$install_dir"/embedded/inspec-sbom.json
+echo "SBOM generated successfully"
