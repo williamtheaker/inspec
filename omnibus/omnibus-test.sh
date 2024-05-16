5aa60798eacc6b6c9b28c44d0798db28e0028877
Sonu Saha <sonu.saha@progress.com>
2024-05-16 12:48:01 +0530

    ci: check git version during omnibus build
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- omnibus/omnibus-test.sh
+++ omnibus/omnibus-test.sh
@@ -5,6 +5,9 @@ export CHEF_LICENSE="accept-no-persist"
 project_root="$(pwd)"
 export project_root
 
+git_version=$(git --version)
+echo "Git version: $git_version"
+
 cd test/artifact
 
 PATH=/opt/inspec/bin:/opt/inspec/embedded/bin:$PATH
